6787a8646d4e303bb5c486e28bf5cf05b44d20e8
pooshpa <pupa.k@outlook.com>
2020-02-29 14:01:22 +0545

    blog

--- single-post.php
+++ single-post.php
@@ -1,119 +1,20 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Politics - A template for politician, political party and election</title>
-
-
-	<!-- FONTS -->
-	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|PT+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
-
-	<!-- STYLESHEET -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
-	<link rel="stylesheet" type="text/css" href="css/ionicons.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-
-</head>
-<body class="page blog-page">
-
-	<!-- SITE HEADER -->
-	<header class="site-header">
-		<div class="top-bar">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-md-6 trending-topics hidden-sm hidden-xs">
-						<h4>Trending</h4>
-						<ul class="list-inline">
-							<li><a href="#">Global Warming</a></li>
-							<li><a href="#">Google</a></li>
-							<li><a href="#">Facebook</a></li>
-						</ul>
-					</div> <!-- .col-md-6 ends -->
-					<div class="col-md-6 social-links">
-						<button class="btn btn-main btn-sm pull-right">Donate Us</button>
-						<ul class="list-inline">
-							<li><a href="#"><i class="icon ion-social-facebook"></i></a></li>
-							<li><a href="#"><i class="icon ion-social-twitter"></i></a></li>
-							<li><a href="#"><i class="icon ion-social-instagram-outline"></i></a></li>
-							<li><a href="#"><i class="icon ion-social-linkedin-outline"></i></a></li>
-						</ul>
-
-					</div> <!-- .col-md-6 ends -->
-				</div> <!-- .row ends -->
-			</div> <!-- .container-fluid ends -->
-		</div> <!-- .top-bar ends -->
-		<nav class="navbar navbar-default politics-navbar">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#politics-navbar-real" area-expanded="false">
-
-						<span class="sr-only">Toggle Navigation</span>
-						<span class="glyphicon glyphicon-menu-hamburger"></span>
-
-					</button>
-
-					<a class="navbar-brand" href="#">Politics</a>
-				</div> <!-- .navbar-header ends -->
-				<div class="collapse navbar-collapse" id="politics-navbar-real">
-					<ul class="nav navbar-nav navbar-left">
-						<li><a href="index.html#site-header">Home <span class="sr-only">(current)</span></a></li>
-						<li><a href="index.html#latest-news">News</a></li>
-						<li><a href="index.html#press-release">Press</a></li>
-						<li><a href="index.html#campaign-events">Campaigns</a></li>
-					</ul> <!-- .nav navbar-nav ends -->
-
-					<ul class="nav navbar-nav navbar-right">
-						
-						<li><a href="index.html#featured-event">Events</a></li>
-						<li><a href="index.html#our-members">Members</a></li>
-						<li class="active dropdown">
-
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
-
-							<ul class="dropdown-menu">
-								<li><a href="blog.html">Blog</a></li>
-								<li><a href="single-post.html">Single Post</a></li>
-							</ul> <!-- .dropdown-menu ends -->
-
-						</li> <!-- .dropdown ends -->
-						<li><a href="contact.html">Contact</a></li>
-					</ul> <!-- .nav navbar-nav ends -->
-				</div> <!-- .collapse navbar-collapse ends -->
-			</div> <!-- .container ends -->
-		</nav> <!-- .navbar navbar-default ends -->
-		
-	</header> <!-- .site-header ends -->
-
-	<section class="page-banner">
-		<div class="container">
-			<h2 class="page-title">Single Post</h2>
-			<ul class="breadcrumbs">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Blog</a></li>
-			</ul> <!-- .breadcrumbs ends -->
-		</div> <!-- .container ends -->
-	</section> <!-- .page-banner ends -->
-
+<?php
+include('header.php');
+?>
 	<section class="page-content section-block">
 		<div class="container">
 			<div class="row section-content">
-				<div class="col-md-9">
+				<div class="col-md-12">
 					<div class="main-content">
 						<div class="blog-posts">
 							<article class="single-post clearfix">
 								<div class="image-block">
-									<img class="img-responsive" src="http://placehold.it/850x567" alt="article image">
+									<img class="img-responsive" src="wall.js/ANPFA image/1.jpg" alt="article image">
 								</div>
 				
 								<div class="post-content">
 									<div class="content-wrapper">
 										<h2 class="post-title">Duis aute irure dolor in repreh enderit in voluptate</h2>
-										<ul class="post-meta">
-											<li><i class="icon ion-person"></i><a href="#">Juwel Khan</a></li>
-											<li><i class="icon ion-pricetag"></i><a href="#">Social Work</a></li>
-											<li><i class="icon ion-chatbubbles"></i><a href="#">23 Comments</a></li>
-										</ul>
 										<div class="content-data">
 											<p>
 												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -168,7 +69,7 @@
 											</p>
 										</div> <!-- .content-data ends -->
 
-										<div class="row post-footer">
+										<!-- <div class="row post-footer">
 											<div class="col-md-6">
 												<ul class="list-inline social-share">
 													<li><a href="#"><i class="icon ion-social-facebook"></i></a></li>
@@ -177,8 +78,8 @@
 													<li><a href="#"><i class="icon ion-social-dribbble"></i></a></li>
 													<li><a href="#"><i class="icon ion-social-tumblr"></i></a></li>
 												</ul>
-											</div> <!-- .col-md-6 ends -->
-											<div class="col-md-6">
+											</div> --> <!-- .col-md-6 ends -->
+											<!-- <div class="col-md-6">
 												<ul class="list-inline post-tags">
 													<li><a href="#">Travel</a></li>
 													<li><a href="#">Social Work</a></li>
@@ -186,191 +87,105 @@
 													<li><a href="#">Lorem Ipsum</li>
 													
 												</ul>
-											</div> <!-- .col-md-6 ends -->
-										</div> <!-- .post-footer ends -->
+											</div> --> <!-- .col-md-6 ends -->
+										<!-- </div> --> <!-- .post-footer ends -->
 									</div> <!-- .content-wrapper ends -->
 								</div> <!-- .post-content ends -->
 							</article> <!-- .blogpost ends -->
 							
-						</div> <!-- .blog-posts ends -->
-						<div class="content-footer prev-next-post clearfix">
+						</div><!-- .blog-posts ends -->
+						<!-- <div class="content-footer prev-next-post clearfix">
 							<a href="#"><i class="icon ion-arrow-left-b"></i> Prev Post</a>
 							<a href="#">Next Post <i class="icon ion-arrow-right-b"></i></a>
-						</div> <!-- .content-footer ends -->
+						</div> --> <!-- .content-footer ends -->
 
-						<div class="comments-block">
+						<!-- <div class="comments-block">
 							<h3 class="block-title">34 Comments</h3>
 							<div class="comments-wrapper">
 								<ul class="comments-list">
 									<li class="comment clearfix">
 										<div class="comment-author">
 											<img class="img-responsive" src="http://placehold.it/84x84" alt="comments author 1">
-										</div> <!-- .comment-author ends -->
+										</div> --> <!-- .comment-author ends -->
 
-										<div class="comment-content">
+										<!-- <div class="comment-content">
 											<div class="comment-meta">
 												<h4><a href="#">Megh o Mala</a></h4>
 												<span>2 Days ago</span>
-											</div> <!-- .comment-meta ends -->
+											</div> --> <!-- .comment-meta ends -->
 
-											<div class="content-data">
+											<!-- <div class="content-data">
 												<p>
 													Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 													tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 													quis nostrud exe. Excepteur sint occaecat cupidatat non
 													proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 												</p>
-											</div> <!-- .content-data ends -->
+											</div> --> <!-- .content-data ends -->
 											
-											<a class="comment-reply-link" href="#">Reply</a>
-										</div> <!-- .comment-content ends -->
+											<!-- <a class="comment-reply-link" href="#">Reply</a>
+										</div> --> <!-- .comment-content ends -->
 										
-										<div class="replied-comments">
+										<!-- <div class="replied-comments">
 											<ul class="comments-list">
 												<li class="comment clearfix">
 													<div class="comment-author">
 														<img class="img-responsive" src="http://placehold.it/84x84" alt="comments author 1">
-													</div> <!-- .comment-author ends -->
+													</div> --> <!-- .comment-author ends -->
 
-													<div class="comment-content">
+													<!-- <div class="comment-content">
 														<div class="comment-meta">
 															<h4><a href="#">Pori bibi</a></h4>
 															<span>2 Days ago</span>
-														</div> <!-- .comment-meta ends -->
+														</div> --> <!-- .comment-meta ends -->
 
-														<div class="content-data">
+														<!-- <div class="content-data">
 															<p>
 																Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 																tempor incid magna aliqua. Ut enim ad mi cupidatat non
 																proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 															</p>
-														</div> <!-- .content-data ends -->
+														</div> --> <!-- .content-data ends -->
 														
-														<a class="comment-reply-link" href="#">Reply</a>
-													</div> <!-- .comment-content ends -->
+														<!-- <a class="comment-reply-link" href="#">Reply</a>
+													</div> --> <!-- .comment-content ends -->
 													
-												</li> <!-- .comment ends -->
+												<!-- </li> --> <!-- .comment ends -->
 
-												<li class="comment clearfix">
+												<!-- <li class="comment clearfix">
 													<div class="comment-author">
 														<img class="img-responsive" src="http://placehold.it/84x84" alt="comments author 1">
-													</div> <!-- .comment-author ends -->
+													</div> --> <!-- .comment-author ends -->
 
-													<div class="comment-content">
+													<!-- <div class="comment-content">
 														<div class="comment-meta">
 															<h4><a href="#">Korimon Nesa</a></h4>
 															<span>2 Days ago</span>
-														</div> <!-- .comment-meta ends -->
+														</div> --> <!-- .comment-meta ends -->
 
-														<div class="content-data">
+														<!-- <div class="content-data">
 															<p>
 																Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 																tempor incididuExcepteur sint occaecat cupidatat non
 																proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 															</p>
-														</div> <!-- .content-data ends -->
+														</div> --> <!-- .content-data ends -->
 														
-														<a class="comment-reply-link" href="#">Reply</a>
-													</div> <!-- .comment-content ends -->
+														<!-- <a class="comment-reply-link" href="#">Reply</a>
+													</div> --> <!-- .comment-content ends -->
 													
-												</li> <!-- .comment ends -->
-											</ul>
-										</div>
-									</li> <!-- .comment ends -->
+												<!-- </li> --> <!-- .comment ends -->
+											<!-- </ul>
+										</div> -->
+									<!-- </li> --> <!-- .comment ends -->
 
-									<li class="comment clearfix">
-										<div class="comment-author">
-											<img class="img-responsive" src="http://placehold.it/84x84" alt="comments author 1">
-										</div> <!-- .comment-author ends -->
-
-										<div class="comment-content">
-											<div class="comment-meta">
-												<h4><a href="#">Mohua Begum</a></h4>
-												<span>2 Days ago</span>
-											</div> <!-- .comment-meta ends -->
-
-											<div class="content-data">
-												<p>
-													Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-													tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-													quis nostrud exe. Excepteur sint occaecat cupidatat non
-													proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-												</p>
-											</div> <!-- .content-data ends -->
-											
-											<a class="comment-reply-link" href="#">Reply</a>
-										</div> <!-- .comment-content ends -->
-										
-									</li> <!-- .comment ends -->
-
-									<li class="comment clearfix">
-										<div class="comment-author">
-											<img class="img-responsive" src="http://placehold.it/84x84" alt="comments author 1">
-										</div> <!-- .comment-author ends -->
-
-										<div class="comment-content">
-											<div class="comment-meta">
-												<h4><a href="#">Anondo Mala</a></h4>
-												<span>2 Days ago</span>
-											</div> <!-- .comment-meta ends -->
-
-											<div class="content-data">
-												<p>
-													Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-													tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-													quis nostrud exe. Excepteur sint occaecat cupidatat non
-													proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-												</p>
-											</div> <!-- .content-data ends -->
-											
-											<a class="comment-reply-link" href="#">Reply</a>
-										</div> <!-- .comment-content ends -->
-										
-									</li> <!-- .comment ends -->
-								</ul> <!-- .comments-list ends -->
-							</div> <!-- .comments-wrapper ends -->
-						</div> <!-- .comments-block ends -->
-
-						<div class="comment-form-block">
-							<h3 class="block-title">Leave a comment</h3>
-
-							<div class="comment-form-wrapper">
-								<form class="comment-form">
-									<div class="row">
-										<div class="col-md-4">
-											<div class="form-group">
-												<label class="sr-only" for="name">Name:</label>
-												<input type="text" class="form-control" placeholder="Your name" id="name">
-											</div> <!-- .form-group ends -->
-										</div> <!-- .col-md-4 ends -->
-										<div class="col-md-4">
-											<div class="form-group">
-												<label class="sr-only" for="name">Name:</label>
-												<input type="text" class="form-control" placeholder="Your name" id="name">
-											</div> <!-- .form-group ends -->
-										</div> <!-- .col-md-4 ends -->
-										<div class="col-md-4">
-											<div class="form-group">
-												<label class="sr-only" for="name">Name:</label>
-												<input type="text" class="form-control" placeholder="Your name" id="name">
-											</div> <!-- .form-group ends -->
-										</div> <!-- .col-md-4 ends -->
-										<div class="col-md-12">
-											<div class="form-group">
-												<label class="sr-only" for="comment">Comment:</label>
-												<textarea class="form-control" placeholder="Your name" id="name"></textarea>
-											</div> <!-- .form-group ends -->
-
-											<button class="btn btn-main pull-right">Submit</button>
-										</div> <!-- .col-md-4 ends -->
-										
-									</div> <!-- .row ends -->
-								</form> <!-- .comment-form ends -->
-							</div> <!-- .comment-form-wrapper ends -->
-						</div> <!-- .comment-form-block ends -->
+									
+								<!-- </ul> --> <!-- .comments-list ends -->
+							<!-- </div> --> <!-- .comments-wrapper ends -->
+						<!-- </div> --> <!-- .comments-block ends -->
 					</div> <!-- .main-content ends -->
 				</div> <!-- .col-md-9 ends -->
-				<div class="col-md-3">
+				<!-- <div class="col-md-3">
 					<div class="sidebar">
 						<div class="widget search-box">
 							<form class="search-form">
@@ -378,43 +193,43 @@
 									<label class="sr-only" for="search-bar">Search:</label>
 									<input class="form-control" type="text" id="search-bar" placeholder="Search" >
 									<button class="search-btn"><i class="icon ion-search"></i> <span class="sr-only">Submit</span></button>
-								</div> <!-- .form-group ends -->
-							</form> <!-- .search-form ends -->
-						</div> <!-- .widget ends -->
-						<div class="widget recent-post">
+								</div>  --><!-- .form-group ends -->
+							<!-- </form> --> <!-- .search-form ends -->
+						<!-- </div> --> <!-- .widget ends -->
+						<!-- <div class="widget recent-post">
 							<h3 class="widget-title">Recent Posts</h3>
 
 							<ul class="sidebar-list list-with-img">
 								<li class="clearfix">
 									<div class="img-block">
 										<img class="img-responsive" src="http://placehold.it/72x72" alt="recent post image">
-									</div> <!-- .img-block ends -->
-									<div class="content-block">
+									</div> --> <!-- .img-block ends -->
+									<!-- <div class="content-block">
 										<h4><a href="#">Belaboris nisi ut aliquip ex ea cois aute irun</a></h4>
 										<span class="post-date">May 25, 2016</span>
-									</div> <!-- .content-block ends -->
-								</li> <!-- .clearfix ends -->
-								<li class="clearfix">
+									</div> --> <!-- .content-block ends -->
+								<!-- </li> --> <!-- .clearfix ends -->
+								<!-- <li class="clearfix">
 									<div class="img-block">
 										<img class="img-responsive" src="http://placehold.it/72x72" alt="recent post image">
-									</div> <!-- .img-block ends -->
-									<div class="content-block">
+									</div> --> <!-- .img-block ends -->
+									<!-- <div class="content-block">
 										<h4><a href="#">Presha darpro tohing sha tomar mukhe die joto</a></h4>
 										<span class="post-date">May 25, 2016</span>
-									</div> <!-- .content-block ends -->
-								</li> <!-- .clearfix ends -->
-								<li class="clearfix">
+									</div> --> <!-- .content-block ends -->
+								<!-- </li> --> <!-- .clearfix ends -->
+								<!-- <li class="clearfix">
 									<div class="img-block">
 										<img class="img-responsive" src="http://placehold.it/72x72" alt="recent post image">
-									</div> <!-- .img-block ends -->
-									<div class="content-block">
+									</div> --> <!-- .img-block ends -->
+									<!-- <div class="content-block">
 										<h4><a href="#">Tobu eideyaler shorire joto rong dhuye jaoa manus</a></h4>
 										<span class="post-date">May 25, 2016</span>
-									</div> <!-- .content-block ends -->
-								</li> <!-- .clearfix ends -->
-							</ul>
-						</div> <!-- .widget ends -->
-						<div class="widget cateogry-block">
+									</div> --> <!-- .content-block ends -->
+								<!-- </li> --> <!-- .clearfix ends -->
+							<!-- </ul>
+						</div> --> <!-- .widget ends -->
+						<!-- <div class="widget cateogry-block">
 							<h3 class="widget-title">Category</h3>
 
 							<ul class="sidebar-list">
@@ -426,8 +241,8 @@
 								<li><a href="#">Campaigns</a></li>
 								<li><a href="#">Events &amp; Seminar</a></li>
 							</ul>
-						</div> <!-- .widget category-block ends -->
-						<div class="widget tags-block">
+						</div>  --><!-- .widget category-block ends -->
+						<!-- <div class="widget tags-block">
 							<h3 class="widget-title">Tags</h3>
 							<ul class="">
 								<li><a href="#">Travel</a></li>
@@ -439,7 +254,7 @@
 								<li><a href="#">Education</a></li>
 								<li><a href="#">GDP</a></li>
 							</ul>
-						</div> <!-- .widget tags-block ends -->
+						</div>  --><!-- .widget tags-block ends -->
 					</div> <!-- .sidebar ends -->
 				</div> <!-- .col-md-3 ends -->
 			</div> <!-- .row ends -->
