f7cd6fe0d79ce7a4de14ebe0aa0c0fbbd2ddaac3
shreeztea <sshristitn@gmail.com>
2020-03-02 22:21:47 +0545

    single post added

--- single-post.php
+++ single-post.php
@@ -1,5 +1,19 @@
 <?php
 include('header.php');
+include "admin/db.php";
+
+if(isset($_GET['id'])){
+	
+		$id = $_GET['id'];
+		$get_con = "SELECT * FROM content where id=".$id;
+
+  		$run_con = mysqli_query($con, $get_con);
+  		// echo $get_con;
+        $row_count = mysqli_num_rows($run_con);
+        // echo $row_count;
+		if($row_count >= 1)
+		{
+			$row_con = mysqli_fetch_array($run_con)
 ?>
 	<section class="page-content section-block">
 		<div class="container">
@@ -9,64 +23,17 @@ include('header.php');
 						<div class="blog-posts">
 							<article class="single-post clearfix">
 								<div class="image-block">
-									<img class="img-responsive" src="wall.js/ANPFA image/1.jpg" alt="article image">
+									<img class="img-responsive" src="admin/img/<?php echo $row_con['image'];?>" alt="article image">
 								</div>
 				
 								<div class="post-content">
 									<div class="content-wrapper">
-										<h2 class="post-title">Duis aute irure dolor in repreh enderit in voluptate</h2>
+										<h2 class="post-title"><?php echo $row_con['title_eng'];?></h2>
 										<div class="content-data">
 											<p>
-												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-												quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-												consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-												cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-												proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+												<?php echo $row_con['desc_eng'];?>
 											</p>
 
-											<p>
-												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-												quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea pariatur. Excepteur sint occaecat cupidatat non
-												proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-											</p>
-
-											<p>
-												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-												quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-												consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-												cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non  ullamco laboris nisi ut aliquip ex ea pariatur. Excepteur sint occaecat cupidatat no  ullamco laboris nisi ut aliquip ex ea pariatur. Excepteur sint occaecat cupidatat no
-												proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-											</p>
-
-											<blockquote>
-												Lorem exercitation ullamco laboris nisi ut aliquip ex ea commodo
-												consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-												cillum dolore eu fugiat nulla pariatur xcepteur sint.
-											</blockquote>
-
-											<p>
-												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-												quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea pariatur. Excepteur sint occaecat cupidatat non
-												proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-											</p>
-
-											<p>
-												Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-												quis nostrud exercitation ullur. Excepteur sint occaecat cupidatat non
-												proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-											</p>
-
-											<p>
-												Lorem ipsum  ullamco laboris nisi ut aliquip ex ea pariatur. Excepteur sint occaecat cupidatat no dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-												tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-												quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea pariatur. Excepteur sint occaecat cupidatat non
-												proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-											</p>
 										</div> <!-- .content-data ends -->
 
 										<!-- <div class="row post-footer">
@@ -260,7 +227,7 @@ include('header.php');
 			</div> <!-- .row ends -->
 		</div> <!-- .container ends -->
 	</section> <!-- .page-content ends -->
-
+<?php }} ?>
 	<footer class="main-footer section-block" id="main-footer">
 		<div class="container">
 			<div class="row section-content">
